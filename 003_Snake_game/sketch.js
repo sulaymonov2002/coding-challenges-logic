1ed2354cbb3ba9a43625447097468131d5d35d8c
sulaymonov2002 <ssamir.web2002@gmail.com>
2023-01-08 22:53:45 +0500

    feat: add new functions for logic

--- 003_Snake_game/sketch.js
+++ 003_Snake_game/sketch.js
@@ -1,12 +1,40 @@
 let s;
+let scl = 20;
+
+let food;
 
 function setup() {
   createCanvas(600, 600);
   s = new Snake();
+  frameRate(10);
+  food = createVector(random(width), random(height));
+}
+
+function pickLocation() {
+  let cols = floor(width / scl);
+  let rows = floor(height / scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
 }
 
 function draw() {
   background(51);
   s.update();
   s.show();
+  s.eat(foot);
+
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
 }
--- 003_Snake_game/snake.js
+++ 003_Snake_game/snake.js
@@ -4,13 +4,25 @@ function Snake() {
   this.xspeed = 1;
   this.yspeed = 0;
 
+  this.dir = function (x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  };
+
+  this.eat = function (pos) {
+    let d = dist(this.x, this.y);
+  };
+
   this.update = function () {
-    this.x = this.x + this.xspeed;
-    this.y = this.y + this.yspeed;
+    this.x = this.x + this.xspeed * scl;
+    this.y = this.y + this.yspeed * scl;
+
+    this.x = constrain(this.x, 0, width - scl);
+    this.y = constrain(this.y, 0, height - scl);
   };
 
   this.show = function () {
     fill(255);
-    rect(this.x, this.y, 10, 10);
+    rect(this.x, this.y, scl, scl);
   };
 }
